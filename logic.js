038f9e88347f0162df6fdeffc4af0ea4ce63c3a2
soyaibur <ovisoyaiburrahman@gmail.com>
2021-11-09 18:58:36 +0600

    initial file to start

---
+++ logic.js
@@ -0,0 +1,18 @@
+alert("Delete me first")
+
+
+
+
+
+
+
+//All code will be deleted here.............
+
+function printOut(text){
+    const div = document.createElement('div')
+    div.innerHTML = text
+    document.body.appendChild(div)
+}
+// addEventListener('click',()=>{
+//     alert("this is working")
+// })
